14b87a3a69fbf55adea3ea7d9a9ffa4a02990c36
Guillaume Kulakowski <guillaume@kulakowski.fr>
2021-06-04 16:14:51 +0200

    Update with 1.36

--- src/SkinFedoraFrVector.php
+++ src/SkinFedoraFrVector.php
@@ -17,39 +17,21 @@ class SkinFedoraFrVector extends SkinVector {
 	public $stylename = 'FedoraFrVector';
 	public $template = 'FedoraFrVectorTemplate';
 
-  /**
-   * @inheritDoc
-   * @return array
-   */
-	public function getDefaultModules() {
-		$modules = parent::getDefaultModules();
+	/**
+	 * @inheritDoc
+	 * @param array|null $options Note; this param is only optional for internal purpose.
+	 * 		Do not instantiate Vector, use SkinFactory to create the object instead.
+	 * 		If you absolutely must to, this paramater is required; you have to provide the
+	 * 		skinname with the `name` key. That's do it with `new SkinVector( ['name' => 'vector'] )`.
+	 * 		Failure to do that, will lead to fatal exception.
+	 */
+	public function __construct( $options = [] ) {
+		$options['scripts'] = [ 'skins.vector.legacy.js' ];
+		$options['styles'] = [ 'skins.vector.styles.legacy', 'skins.fedorafrvector.styles' ];
 
-		$modules[Constants::SKIN_NAME] = 'skins.fedorafrvector.js';
-		$modules['styles'] = array_merge(
-			$modules['styles'],
-			[ 'skins.fedorafrvector.styles' ]
-		);
+		$options['templateDirectory'] =  __DIR__ . '/../templates';
+		$options[Constants::SKIN_NAME] = 'skins.fedorafrvector.js';
 
-		return $modules;
-	}
-
-  /**
-   * @inheritDoc
-   *
-   * @param string $classname
-   * @return VectorTemplate
-   */
-	protected function setupTemplate( $classname ) {
-		$tp = new TemplateParser( __DIR__ . '/../templates' );
-		return new \VectorTemplate( $this->getConfig(), $tp, $this->isLegacy() );
-	}
-
-  /**
-   * Force no legacy.
-   *
-   * @return bool
-   */
-	private function isLegacy() : bool {
-		return false;
+		SkinMustache::__construct( $options );
 	}
 }
